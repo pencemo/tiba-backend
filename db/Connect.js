27a178d20806eeeab8160a80489438850a8f01d8
unknown <applenetdesign@gmail.com>
2025-02-05 18:47:54 +0530

    add new cofig

--- db/Connect.js
+++ db/Connect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const url = process.env.DB_URL;
+const url = process.env.MONGODB_URI;
 const name = process.env.DB_NAME;
 const dev = process.env.NODE_ENV === "development";
 const connectUrl = dev ? "mongodb://localhost:27017/myapp" : url+name;
