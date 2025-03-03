fbdafd5989d3e71b6e65da8b6dc702429e97058b
unknown <applenetdesign@gmail.com>
2025-03-03 14:04:48 +0530

    add mongodb url

--- db/Connect.js
+++ db/Connect.js
@@ -5,10 +5,10 @@ dotenv.config();
 const url = process.env.MONGODB_URI;
 const name = process.env.DB_NAME;
 const dev = process.env.NODE_ENV === "development";
-const connectUrl = dev ? "mongodb://localhost:27017/myapp" : url+name;
+const connectUrl = dev ? "mongodb://localhost:27017/myapp" : url;
 export const dbConnection = async () => {
     try{
-        await mongoose.connect(connectUrl)
+        await mongoose.connect(url)
             .then(() => console.log("Connected to DB"))
             .catch((err) => console.log(err));
     }catch(err){
